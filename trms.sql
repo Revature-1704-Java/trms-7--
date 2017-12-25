efc03f6f78266ee36fd13919e9ccff3ce8902f9a
Phil <regop412@gmail.com>
2017-12-25 13:49:21 -0500

    save work

--- trms.sql
+++ trms.sql
@@ -5,19 +5,19 @@ CREATE TABLE Department
 (
     DepartmentId NUMBER NOT NULL,
     Name VARCHAR2(160) NOT NULL,
-    Head NUMBER NOT NULL,
+    Head NUMBER,
     
     CONSTRAINT PK_Department PRIMARY KEY  (DepartmentId)
 );
-CREATE SEQUENCE DepartmentId MINVALUE 1 START WITH 1 INCREMENT BY 1 CACHE 20;
 
 
+INSERT INTO EducationType(EducationTypeId, Name, PercentCovered) VALUES (5, 'Technical Training', .9 );
 CREATE TABLE Employee
 (
     EmployeeId NUMBER NOT NULL , 
     LastName VARCHAR2(200) NOT NULL,
     FirstName VARCHAR2(200) NOT NULL,
-    Supervisor NUMBER NOT NULL,
+    Supervisor NUMBER,
     Department NUMBER NOT NULL,
     IsBenefitCoodinator CHAR(1) DEFAULT 0 NOT NULL, 
     Email VARCHAR2(200) NOT NULL,
@@ -34,10 +34,6 @@ CREATE TABLE Employee
     CONSTRAINT PK_Employee PRIMARY KEY  (EmployeeId),
     CONSTRAINT EmployeeEmail_unique UNIQUE (Email)
 );
-CREATE SEQUENCE EmployeeId MINVALUE 1 START WITH 1 INCREMENT BY 1 CACHE 20;
-INSERT INTO EMPLOYEE (LastName, FirstName, Supervisor, Department, Email) VALUES ('Rego', 'Philip', 1, 1,);
-
-
 
 CREATE TABLE EducationRequest
 (
@@ -65,7 +61,7 @@ CREATE TABLE EducationType
     Name VARCHAR2(200),
     PercentCovered VARCHAR2(200),
 
-    CONSTRAINT PK_Employee PRIMARY KEY (EmployeeId)
+    CONSTRAINT PK_EducationType PRIMARY KEY (EducationTypeId)
 );
 
 
@@ -83,9 +79,33 @@ ALTER TABLE Employee ADD CONSTRAINT FK_EmployeeDepartment
     
     
 
+/*******************************************************************************
+   Populate Tables
+********************************************************************************/
+
+INSERT INTO DEPARTMENT (DepartmentId, Name) VALUES(1,'Software');
+INSERT INTO DEPARTMENT (DepartmentId, Name) VALUES(2,'Human Resources');
+INSERT INTO DEPARTMENT (DepartmentId, Name) VALUES(3,'Marketing');
+INSERT INTO DEPARTMENT (DepartmentId, Name) VALUES(4,'Production');
+
+INSERT INTO Employee (EmployeeId, LastName, FirstName, Department, Email) VALUES (1, 'Rego', 'Philip', 1, 'regop412@gmail.com');
+INSERT INTO Employee (EmployeeId, LastName, FirstName, Department, Email) VALUES (2, 'Adams', 'Sneezy', 1, 'sneezya@gmail.com');
+INSERT INTO Employee (EmployeeId, LastName, FirstName, Department, Email) VALUES (3, 'Baker', 'Sleepy', 2, 'sleepyb@gmail.com');
+INSERT INTO Employee (EmployeeId, LastName, FirstName, Department, Email) VALUES (4, 'Clark', 'Dopey', 3, 'regopdopeyc@gmail.com');
+INSERT INTO Employee (EmployeeId, LastName, FirstName, Department, Email) VALUES (5, 'Patel', 'Grumpy', 4, 'grumpyp@gmail.com');
+INSERT INTO Employee (EmployeeId, LastName, FirstName, Department, Email) VALUES (6, 'Xiang', 'Bashful', 4, 'bashfulx@gmail.com');
+
+INSERT INTO EducationType(EducationTypeId, Name, PercentCovered) VALUES (1, 'University Courses', .8 );
+INSERT INTO EducationType(EducationTypeId, Name, PercentCovered) VALUES (2, 'Seminars', .6 );
+INSERT INTO EducationType(EducationTypeId, Name, PercentCovered) VALUES (3, 'Certification Preparation Classes', .75 );
+INSERT INTO EducationType(EducationTypeId, Name, PercentCovered) VALUES (4, 'Certification', 1 );
+INSERT INTO EducationType(EducationTypeId, Name, PercentCovered) VALUES (5, 'Technical Training', .9 );
+INSERT INTO EducationType(EducationTypeId, Name, PercentCovered) VALUES (6, 'Other', .3 );
 
 
-
+/*******************************************************************************
+   Assign department heads  
+********************************************************************************/
 
 
 
